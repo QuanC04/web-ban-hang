fe12f61a2939ca8acacf3160ce3c2c7f4ba9686c
DinhQuan14022004 <dinhquan14022004@gmail.com>
2026-04-22 23:05:44 +0700

    feat: add authentication middleware to logout route

--- be/src/modules/auth/auth.route.ts
+++ be/src/modules/auth/auth.route.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express';
 import { registerController, loginController, logoutController } from './auth.controller';
+import { authenticate } from '../../middleware/authicate';
 
 const authRoutes: Router = Router();
 
 authRoutes.post('/register', registerController);
 authRoutes.post('/login', loginController);
-authRoutes.post('/logout', logoutController);
+authRoutes.post('/logout',authenticate, logoutController);
 
 export default authRoutes;
