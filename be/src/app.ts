9e47b6afa719a731fcda49c8aa05238df0ec760e
DinhQuan14022004 <dinhquan14022004@gmail.com>
2026-05-19 00:04:15 +0700

    feat: update CORS configuration to include additional allowed origin

--- be/src/app.ts
+++ be/src/app.ts
@@ -22,7 +22,10 @@ app.use(helmet());
 
 app.use(
     cors({
-        origin: process.env.CLIENT_URL || 'http://localhost:5173',
+        origin:
+            process.env.CLIENT_URL ||
+            'http://localhost:5173' ||
+            'https://web-ban-hang-zeta.vercel.app',
         credentials: true,
     }),
 );
